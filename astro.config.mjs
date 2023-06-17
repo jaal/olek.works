a693c13fe09307296ca5ea2bc60dc2e1bd16e086
ajaworski <ajaworski@opera.com>
2023-06-17 21:27:14 +0200

    2023-06-17: add astro; add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
   site: 'https://olek.works',
   trailingSlash: 'always',
   markdown: {
     shikiConfig: {
-      theme: 'material-lighter',
-    },
-  },
+      theme: 'material-lighter'
+    }
+  }
 });
